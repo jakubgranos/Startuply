6034ee994d7d4ad869da6839e652730582f42a05
Jakub <jakubgranos@gmail.com>
2018-11-21 18:38:45 +0100

    modyfikacja menu - dodanie sticky oraz scrollto

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,8 @@
 import swiper from './modules/slider';
 import './modules/showing-menu.js'; 
 import './modules/video.js'; 
+import './modules/scrol-menu.js'; 
+import './modules/sticky.js'; 
 
 import getWindowUserAgent from './modules/userAgent';
 window.onload = () => {
---
+++ src/assets/js/modules/scrol-menu.js
@@ -0,0 +1,22 @@
+jQuery(function($) {
+	$.scrollTo(0);
+	$('#link1').click(function() {
+		$.scrollTo($('#home'), 1000);
+	});
+	$('#link2').click(function() {
+		$.scrollTo($('#about'), 1000);
+	});
+	$('#link3').click(function() {
+		$.scrollTo($('#about'), 1000);
+	});
+	$('#link4').click(function() {
+		$.scrollTo($('#about'), 1000);
+	});
+	$('#link5').click(function() {
+		$.scrollTo($('#blog'), 1000);
+	});
+	$('#link6').click(function() {
+		$.scrollTo($('#contact'), 1000);
+	});
+});
+	
